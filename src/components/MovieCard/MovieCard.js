dac88f7e243e0e1115ff63ef460860a003144ad1
Lucas Silva <martssilvapro@gmail.com>
2021-09-22 09:09:10 -0300

    adiciona estilo para cards

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import './movieCard.css';
 
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { title, storyline, id } = movie;
+    const { title, storyline, imagePath, id } = movie;
     return (
-      <div data-testid="movie-card">
-        <p>{title}</p>
-        <p>{storyline}</p>
-        <Link to={ `/movies/${id}` }>
+      <div className="card" data-testid="movie-card">
+        <img className="image-card" src={ imagePath } />
+        <p className="card-title">{title}</p>
+        <p className="card-storyline">{storyline}</p>
+        <Link className="card-link" to={ `/movies/${id}` }>
           VER DETALHES
         </Link>
       </div>
