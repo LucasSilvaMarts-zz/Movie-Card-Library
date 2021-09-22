c8a9cef41431e9ebb9de84ae911af16a43a6b79f
Lucas Silva <martssilvapro@gmail.com>
2021-09-22 10:05:59 -0300

    adiciona estilo de loading

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import './loading.css';
 
 class Loading extends Component {
   render() {
     return (
-      <div><h1>Carregando...</h1></div>
+      <div className="loading-section">
+        <img src="https://media.wired.com/photos/592744d3f3e2356fd800bf00/master/w_2560%2Cc_limit/Netflix_LoadTime.gif" alt="Carregando..." />
+      </div>
     );
   }
 }
